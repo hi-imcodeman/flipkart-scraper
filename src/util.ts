25d453c7afc88d8f722a2f3bbb4917455f46524b
Asraf Ali <asraf.cse@gmail.com>
2021-02-20 16:53:00 +0530

    feat: stats implemeted

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,11 @@
-export const sleep = (ms: number): Promise<void> => {
+import numeral from 'numeral'
+
+export const sleep = async (ms: number): Promise<void> => {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve()
         }, ms)
     })
 }
+
+export const durationFormatWithMs = (ms: number):string => `${numeral(ms / 1000).format('00:00:00')} ${ms % 1000}ms`
